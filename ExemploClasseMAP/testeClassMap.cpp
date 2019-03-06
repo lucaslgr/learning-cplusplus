9d17d6f56a6fac50fb178409eb93644ddcd8b3e7
Lucas Guimaraes <lucaslgr1206@gmail.com>
2019-03-06 00:20:30 -0300

    Terminado o estudo da classe map

--- ExemploClasseMAP/testeClassMap.cpp
+++ ExemploClasseMAP/testeClassMap.cpp
@@ -91,8 +91,105 @@ int main(int agrc, const char *agrv[])
     cout << "\n\n\n\n" ;
 
     //Mostrando todos elementos
+    /*
+        o iterator de um map acessa a chave de um elemento com o método first e acessa o valor
+        do elemento com o método second, ex:
+
+            std::map<int, string>::iterator it;
+            it = mapa.begin();
+
+            cout << "Printando a chave: " << it->first" << endl;
+            cout << "Printando o valor: " << it->second << endl;
+    */
     for (it = mapa1.begin(); it != mapa1.end(); it++)
         cout << "A chave " << it->first << " => " << it->second << endl;
+
+
+    //Testando se uma chave existe usando a função find que busca em todos os elementos até o último caso não encontre
+    if (mapa1.find(2) == mapa1.end())
+        cout << "\nChave 2 NAO existe!\n";
+    else
+        cout <<"\nChave 2 existe!\n";
+    
+    
+    /* Inserindo elementos, 1ª forma:
+            ->Usando a função insert para inserir elementos de um map em uma determinada posicao de acordo com a sua chave
+    */
+    // it = mapa1.begin(); //Pegando endereço para o início do map
+    // mapa1.insert(pair<int, string>(2, "maria")); //Usando a funcao pair para criar um elemento da map, ou seja, um par
+
+    /* Inserindo elementos, 2ª forma:
+            ->Usando o operador [] e passando a key e o valor do elemento pelo operador de atribuição =, exemplo:
+                mapa[1 /KEY/ ] = "lucas" /Valor/ 
+    */
+
+    mapa1[2] = "Maria"; //Inserindo no map, o valor Maria em um elemento com a key = 2
+
+    //Mostrando todos elementos de um map usando o iterator
+    for(it = mapa1.begin(); it != mapa1.end(); it++)
+        cout << "A chave " << it->first << " => " << it->second << endl;
+    cout << endl << endl << endl;
+
+
+    /*
+        - Multimap, são mapas onde você pode ter vários elementos com chaves equivalentes
+        - Utilizando multimap:
+            -> Declaração, ex:
+                multimap<int, string> mm;
+
+                mm.insert(pair<int, string>(1, "python 2 "));
+                mm.insert(pair<int, string>(1, "python 3 "));
+    */
+
+    multimap<int, string> mm; //Declarando um multimap
+
+    //Inserindo elementos através do pair
+    mm.insert(pair<int, string>(1, "python 2"));
+    mm.insert(pair<int, string>(1, "python 3"));
+    mm.insert(pair<int, string>(2, "C"));
+    mm.insert(pair<int, string>(2, "C++"));
+    mm.insert(pair<int, string>(3, "ruby"));
+
+    multimap<int, string>::iterator it_mm = mm.begin();
+
+    /*
+        - Lembrando que o iterator é um ponteiro que aponta para o endereço de memória de um objeto map neste caso
+    */
+
+    while(it_mm != mm.end())
+    {
+        cout << "Key => " << (*it_mm).first << " Valor => " << (*it_mm).second << endl;
+        it_mm++;
+    }
+
+     cout << endl << endl << endl;
+    // FIM - multimap
+
+
+
+    /*
+        Funções da map, lower_bound(min_key) e upper_bound(max_key)
+            Descrição: retorna o endereço para os elementos cujas as chaves vão dos limites específicados como mínimo no 
+            lower_bound e máximo no upper_bound
+    */
+
+    multimap<int, string> mm_1; //Declarando um multimap
+
+    //Inserindo elementos através do pair
+    mm_1.insert(pair<int, string>(1, "python 2"));
+    mm_1.insert(pair<int, string>(1, "python 3"));
+    mm_1.insert(pair<int, string>(3, "C"));
+    mm_1.insert(pair<int, string>(4, "C++"));
+    mm_1.insert(pair<int, string>(4, "ruby"));
+
+    multimap<int, string>::iterator it_low, it_up, it_mm_AUX; //Declarando os iteradores
+
+    it_low = mm_1.lower_bound(2); //Atribuindo o endereço do elemento cuja chave = 2
+    it_up = mm_1.upper_bound(4);  //Atribuindo o endereço do elemento cuja chave = 4
+
+    //Imprimindo elementos referenciados de it_low até it_up
+    for (it_mm_AUX = it_low; it_mm_AUX != it_up; ++it_mm_AUX)
+        cout << it_mm_AUX->first << " => " << it_mm_AUX->second << endl;
 
     return 0;
 }
